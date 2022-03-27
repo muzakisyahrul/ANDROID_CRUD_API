bf4c926f6cf11f182b61f36b7c1f0ad7e52a0b3b
Muzaki <muzakisyahrul100@gmail.com>
2022-03-27 09:36:24 +0700

    Initial commit

---
+++ app/src/main/java/mobile/muzaki/crudapi/MainActivity.kt
@@ -0,0 +1,11 @@
+package mobile.muzaki.crudapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
